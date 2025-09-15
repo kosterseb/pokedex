f8dbc428410fd4a62449fe54db74dbad969d1323
kosterseb <sebkos.code@gmail.com>
2025-09-15 14:11:22 +0200

    changes to the pokedex

--- pokedex/js/pokedex.js
+++ pokedex/js/pokedex.js
@@ -225,7 +225,7 @@ document.getElementById('searchInput').addEventListener('input', (e) => {
 // Load Pokemon data
 async function loadPokemonData() {
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=900');
         const data = await response.json();
         
         const promises = data.results.map(async (pokemon) => {
